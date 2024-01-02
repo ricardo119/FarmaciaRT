ee21ea19e91173cb569d43a757bd83a1375ac6cd
tiagocambao13 <tiagocambao@gmail.com>
2024-01-02 18:12:20 +0000

    print 1

--- src/farmaciart/FarmaciaRT.java
+++ src/farmaciart/FarmaciaRT.java
@@ -8,8 +8,46 @@ public class FarmaciaRT {
     
     public static void main(String[] args) {
  
-         System.out.println("print");
-         System.out.println("print");
+       Scanner scanner = new Scanner(System.in);
+
+        int escolha;
+        do {
+            System.out.println(" Farmácia R&T!");
+            System.out.println("Selecione uma opção:");
+            System.out.println("1. Menu Administrador");
+            System.out.println("2. Lista de Produtos");
+            System.out.println("3. Lista de Clientes");
+            System.out.println("4. Realizar Venda");
+            System.out.println("0. Sair");
+
+            escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    menuAdministrador();
+                    break;
+                case 2:
+                    Produtos();
+                    break;
+                case 3:
+                    listarClientes();
+                    break;
+                case 4:
+                    realizarVendas();
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema. Até logo!");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, selecione novamente.");
+                    break;
+            }
+        } while (escolha != 0);
+        
+        scanner.close();
     }
+
     
-}
+    
+    
+
--- src/farmaciart/Produto.java
+++ src/farmaciart/Produto.java
@@ -2,6 +2,10 @@
 
 package farmaciart;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Produto {
     
@@ -22,6 +26,33 @@ public class Produto {
         
     }
     
+    
+    public void salvarProduto() {
+        String nomeArquivo = "produtos.txt"; // Nome do arquivo de dados
+
+        try {
+            File arquivo = new File(nomeArquivo);
+
+            // Se o arquivo não existir, cria um novo
+            if (!arquivo.exists()) {
+                arquivo.createNewFile();
+            }
+
+            // Abre o arquivo para escrita
+            FileWriter fw = new FileWriter(arquivo, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            // Escreve os detalhes do novo produto no arquivo
+            bw.write("Nome: " + nome + ", Preço: " + preco);
+            bw.newLine(); // Pula para a próxima linha para o próximo produto
+            bw.close(); // Fecha o arquivo
+
+            System.out.println("Produto adicionado com sucesso ao arquivo!");
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar o produto: " + e.getMessage());
+        }
     }
+}
+    
     
 
