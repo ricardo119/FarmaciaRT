27c9047d909547d32bae8d147100543158cf54b8
tiagocambao13 <tiagocambao@gmail.com>
2024-01-11 11:55:16 +0000

    Atualização cliente, farmacia e morada

---
+++ src/farmaciart/Atendimento.java
@@ -0,0 +1,25 @@
+
+package farmaciart;
+
+import java.io.Serializable;
+
+
+
+
+public class Atendimento extends Cliente implements Serializable{
+    
+     
+ 
+    public Atendimento(String nome, String nif, String rua, String postal, String localidade, boolean isVisivel){
+    
+        super(nome, nif, rua, postal, localidade, isVisivel);
+}
+
+        
+        
+        
+    }
+
+
+   
+
